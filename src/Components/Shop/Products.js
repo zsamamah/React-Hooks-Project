569160d5266182a5d006f001c3985a839ad23aee
Tharaa Ibrahim <jamil.tharaa@gmail.com>
2021-12-25 00:15:48 +0200

    Cars Gallery - Categories -Filter

--- src/Components/Shop/Products.js
+++ src/Components/Shop/Products.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import ProductItem from './ProductItem';
+import React from "react";
+import ProductItem from "./ProductItem";
+import cars from "./cars.json";
 
 export function Products(props) {
-
-        if (!props.items) {
-            return (<h1 className='shop-text-center'>No Products Yet!</h1>);
-        } else
+  //   if (!props.items) {
+  //     return <h1 className="shop-text-center">No Products Yet!</h1>;
+  //   } else
+  return (
+    <div className="shop-card-grid">
+      {cars.map((car) => {
+        <ProductItem
+          id={car.id}
+          name={car.name}
+          price={car.price}
+          src={car.img}
+        />;
+      })}
+      {/* {!props.showDelete&&
+                  props.items.map((item,index) => {
+                      return <ProductItem showDelete={props.showDelete} deleteCard={props.deleteCard} num={index} key={item.id} item={item} />
+                  })
+              } */}
+      {/* {props.showDelete &&
+          props.items.map((item, index) => {
             return (
-                <div className='shop-card-grid'>
-                    {!props.showDelete&&
-                        props.items.map((item,index) => {
-                            return <ProductItem showDelete={props.showDelete} deleteCard={props.deleteCard} num={index} key={item.id} item={item} />
-                        })
-                    }
-                    {props.showDelete&&
-                        props.items.map((item,index) => {
-                            return <ProductItem showDelete={props.showDelete} deleteCard={props.deleteCard} num={index} key={item.id} item={item} />
-                        })
-                    }
-                </div>
-            )
-    }
+              <ProductItem
+                showDelete={props.showDelete}
+                deleteCard={props.deleteCard}
+                num={index}
+                key={item.id}
+                item={item}
+              />
+            );
+          })} */}
+    </div>
+  );
+}
 
-export default Products
-
+export default Products;
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,23 +1,125 @@
-import React ,{useState} from 'react';
-import Products from './Products';
-import Hero from '../Hero/Hero';
+import React, { useState } from "react";
+import Products from "./Products";
+import Hero from "../Hero/Hero";
 import "./Shop.css";
+import ProductItem from "./ProductItem";
+import cars from "./cars.json";
 
+export function Shop() {
+  const [carArray, setCarArray] = useState(cars);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [value, setValue] = useState("");
 
-export function Shop (props) {
-    const [state,setState]=useState({ items:JSON.parse(localStorage.getItem('items'))})
-    
-    return (
-        <>
-            <Hero title="Shop Page"/>
-                <div className='shop-container'>
-                    <Products items={state.items} />
-                </div>
-            </>
-        )
+  const categoryHandle = (category) => {
+    switch (category) {
+      case "Mercedes":
+        setCarArray(cars.filter((car) => car.name === "Mercedes"));
+        setSelectedCategory("Mercedes");
+        break;
+      case "Audi":
+        setCarArray(cars.filter((car) => car.name === "Audi"));
+        setSelectedCategory("Audi");
+        break;
+      case "Toyota":
+        setCarArray(cars.filter((car) => car.name === "Toyota"));
+        setSelectedCategory("Toyota");
+        break;
+      case "Mitsubishi":
+        setCarArray(cars.filter((car) => car.name === "Mitsubishi"));
+        setSelectedCategory("Mitsubishi");
+        break;
+      case "Fiat":
+        setCarArray(cars.filter((car) => car.name === "Fiat"));
+        setSelectedCategory("Fiat");
+        break;
+      default:
+        setCarArray(cars);
+        setSelectedCategory("");
     }
-    
-    
-    export default Shop
-    
-    
+  };
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    switch (e.target.value) {
+      case "Low price":
+        setCarArray(
+          carArray.sort((firstCar, nextCar) =>
+            firstCar.price > nextCar.price ? 1 : -1
+          )
+        );
+        break;
+      case "High price":
+        setCarArray(
+          carArray.sort((firstCar, nextCar) =>
+            nextCar.price > firstCar.price ? 1 : -1
+          )
+        );
+
+        break;
+
+      default:
+        setCarArray(carArray);
+        break;
+    }
+  };
+  return (
+    <>
+      <h1>Gallery</h1>
+      <div className="shop-container">
+        <div className="categories-container">
+          <p>Categories</p>
+          <ul className="categories">
+            <li onClick={() => categoryHandle("")}>All Cars</li>
+            <li onClick={() => categoryHandle("Mercedes")}>Mercedes</li>
+            <li onClick={() => categoryHandle("Audi")}>Audi</li>
+            <li onClick={() => categoryHandle("Toyota")}>Toyota</li>
+            <li onClick={() => categoryHandle("Mitsubishi")}>Mitsubishi</li>
+            <li onClick={() => categoryHandle("Fiat")}>Fiat</li>
+          </ul>
+          <p>Price Filter</p>
+          <select id="rating-filter" value={value} onChange={handleChange}>
+            <option
+              style={{ background: "white", color: "black" }}
+              value="Sort"
+            >
+              Sort
+            </option>
+            <option
+              style={{ background: "white", color: "black" }}
+              value="Low price"
+            >
+              Low to High
+            </option>
+            <option
+              style={{ background: "white", color: "black" }}
+              value="High price"
+            >
+              High to Low
+            </option>
+          </select>
+        </div>
+        <div className="car-gallery">
+          <p>Gallery/{selectedCategory}</p>
+          {carArray.map((car) => {
+            return (
+              <ProductItem
+                key={car.id}
+                id={car.id}
+                name={car.name}
+                price={car.price}
+                src={car.img}
+                model={car.model}
+                seats={car.seats}
+                gear={car.gear}
+                color={car.color}
+                year={car.year}
+                alt={car.alt}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Shop;
